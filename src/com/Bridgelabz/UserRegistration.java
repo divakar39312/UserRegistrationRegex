6c1cba298641c5c22d363c33aebe00851cf08226
Divakar <divakar39312@gmail.com>
2022-09-07 09:25:56 +0530

    [Divakar]:valid password

--- src/com/Bridgelabz/UserRegistration.java
+++ src/com/Bridgelabz/UserRegistration.java
@@ -7,7 +7,8 @@ public class UserRegistration {
 
 
 	static Scanner sc = new Scanner(System.in);
-		public static void validFirstName() {
+	
+	public static void validFirstName() {
 	
 		System.out.println("enter your first name");
 		String firstName = sc.next();
@@ -18,7 +19,8 @@ public class UserRegistration {
 			System.err.println(firstName+" is an Invalid First Name");
         System.out.println(" ");
 	}
-		
+	
+	
 	public static void validLastName() {
 		
 		System.out.println("enter last name");
@@ -30,7 +32,7 @@ public class UserRegistration {
 			System.err.println(lastName+" is an Invalid Last Name\n");
 	}
 	/*
-	 * UC3 : as a user you need to enter valid email
+	 * UC3 check for  valid email
 	 * e.g. abc.xyz@bl.co.in
 	 * email has 3 mandatory fields abc,bl,co and 2 optional in,xyz
 	 */
@@ -38,34 +40,52 @@ public class UserRegistration {
 	{
 	     System.out.println("enter your mail: ");
 	      String mail = sc.next();
-	      boolean result = Pattern.matches("^[a-zA-Z]+[a-zA-Z0-9]*[- . + _]?[a-zA-Z0-9]+[@]{1}[a-z0-9]+[.]{1}[a-z]+[.]?[a-z]+$",mail);  
+	      boolean result = Pattern.matches("^[a-zA-Z]+[0-9]*[- . + _]?[a-zA-Z0-9]+[@]{1}[a-z0-9]+[.]{1}[a-z]{2,}[.]?[a-z]{2,}$",mail);  
 	      if(result) {
 	         System.out.println("mail is valid");
 	      } else {
 	         System.out.println("mail is not valid");
 	      }
-
+	      
 	}
-	// check for valid mobile number
+	/*
+	 * UC4 : as user you need to enter valid mobile number
+	 */
 	public static void validMobileNum()
 	{
 	     System.out.println("enter your mobile number: ");
 	      String mobileNum = sc.next();
-	      boolean result = Pattern.matches("^[+0-9]{3}[0-9]{10}$",mobileNum);  
+	      boolean result = Pattern.matches("^[[+0-9]{3}]?[0-9]{10}$",mobileNum);  
 	      if(result) {
 	         System.out.println("number is valid");
 	      } else {
 	         System.out.println("number is not valid");
 	      }
-
+	}
+	/*
+	 *fined password rules
+	 * rule 1 : password must have minimum 8 characters
+	 */
+	public static void validPassword()
+	{
+	     System.out.println("enter a password(min 8 characters): ");
+	      String password = sc.next();
+	      boolean result = Pattern.matches("^[[a-zA-Z0-9]*[@!#$^&*?]*[a-zA-Z0-9]]{8,}+$",password);  
+	      if(result) {
+	         System.out.println("password is valid");
+	      } else {
+	         System.out.println("password is not valid");
+	      }
+	      validPassword();
 	}
 
 public static void main(String[] args) {
-	System.out.println("welcome to user registration program ");	
+	
 		validFirstName();
 		validLastName();
 		validMail();
 		validMobileNum();
+		validPassword();
 	}
 }
 
