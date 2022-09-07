e4b6504708887c8f39a612b1c99ee769e28ac25f
Divakar <divakar39312@gmail.com>
2022-09-07 08:47:58 +0530

    [Divakar]:check for last name

--- src/com/Bridgelabz/UserRegistration.java
+++ src/com/Bridgelabz/UserRegistration.java
@@ -5,9 +5,8 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
 
-
 		static Scanner sc = new Scanner(System.in);
-		//firstname validation
+		// UC1 check for valid first name
 		public static void validFirstName() {
 		
 			System.out.println("enter your first name");
@@ -17,12 +16,25 @@ public class UserRegistration {
 				System.out.println(firstName+" is a Valid First Name");
 			else
 				System.err.println(firstName+" is an Invalid First Name");
+	        System.out.println(" ");
 		}
+		//UC2 check for valid last name
 		
-	public static void main(String[] args) {
-		System.out.println("welcome to user registration program ");	
-			validFirstName();
+		public static void validLastName() {
+			
+			System.out.println("enter last name");
+			String lastName = sc.next();
+		    boolean result = Pattern.matches("[A-Z]{1}[a-z]{2,}",lastName);
+	        if(result)
+				System.out.println(lastName+" is a Valid Last Name");
+			else
+				System.err.println(lastName+" is an Invalid Last Name\n");
 		}
 
+	public static void main(String[] args) {
+		System.out.println("welcome to user registration");	
+			validFirstName();
+			validLastName();
+		}
 }
 
