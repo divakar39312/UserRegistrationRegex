d0a2d839c84e80864c4fd5a69c00f65134b0d128
Divakar <divakar39312@gmail.com>
2022-09-07 10:08:50 +0530

    [Divakar]:atleast 1 numeric

--- src/com/Bridgelabz/UserRegistration.java
+++ src/com/Bridgelabz/UserRegistration.java
@@ -17,8 +17,7 @@ public class UserRegistration {
 			System.err.println(firstName+" is an Invalid First Name");
         System.out.println(" ");
 	}
-	
-	
+		
 	public static void validLastName() {
 		
 		System.out.println("enter last name");
@@ -30,7 +29,7 @@ public class UserRegistration {
 			System.err.println(lastName+" is an Invalid Last Name\n");
 	}
 	/*
-	 * UC3 : as a user you need to enter valid email
+	 * UC3 :  enter valid email
 	 * e.g. abc.xyz@bl.co.in
 	 * email has 3 mandatory fields abc,bl,co and 2 optional in,xyz
 	 */
@@ -38,7 +37,7 @@ public class UserRegistration {
 	{
 	     System.out.println("enter your mail: ");
 	      String mail = sc.next();
-	      boolean result = Pattern.matches("^[a-zA-Z]+[0-9]*[- . + _]?[a-zA-Z0-9]+[@]{1}[a-z0-9]+[.]{1}[a-z]{2,}[.]?[a-z]{2,}$",mail);  
+	      boolean result = Pattern.matches("^[a-zA-Z]+[a-zA-Z0-9]*[- . + _]?[a-zA-Z0-9]+[@]{1}[a-z0-9]+[.]{1}[a-z]+[.]?[a-z]+$",mail);  
 	      if(result) {
 	         System.out.println("mail is valid");
 	      } else {
@@ -47,13 +46,13 @@ public class UserRegistration {
 	      
 	}
 	/*
-	 * UC4 : as user you need to enter valid mobile number
+	 * UC4 enter valid mobile number
 	 */
 	public static void validMobileNum()
 	{
 	     System.out.println("enter your mobile number: ");
 	      String mobileNum = sc.next();
-	      boolean result = Pattern.matches("^[[+0-9]{3}]?[0-9]{10}$",mobileNum);  
+	      boolean result = Pattern.matches("^[+0-9]{3}[0-9]{10}$",mobileNum);  
 	      if(result) {
 	         System.out.println("number is valid");
 	      } else {
@@ -61,24 +60,28 @@ public class UserRegistration {
 	      }
 	}
 	/*
-	 * UC 6 : as a user need to follow pre-defined password rules
-	 * rule 1 : password must have 1 UpperCase character
+	 * UC 7 : as a user need to follow pre-defined password rules
+	 * rule 3 : password must have atleast 1 numeric value
 	 */
 	public static void validPassword()
 	{
 	     System.out.println("enter a password(min 8 characters): ");
 	      String password = sc.next();
-	      boolean result = Pattern.matches("^(?=.*[A-Z])([a-z0-9]*([@#$%^&?*])*).{8,}$",password);  
+	      boolean result = Pattern.matches("^(?=.*[A-Z])(?=.*[0-9])([a-z0-9]*([@#$%^&?*])*).{8,}$",password);  
 	      if(result) {
 	         System.out.println("password is valid");
 	      } else {
 	         System.out.println("password is not valid");
 	      }
-	      validPassword();
+	      
 	}
 
 public static void main(String[] args) {
 	
+		validFirstName();
+		validLastName();
+		validMail();
+		validMobileNum();
 		validPassword();
 	}
 }
